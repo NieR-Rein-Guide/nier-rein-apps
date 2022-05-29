43a6183ea52f351cd02c8cb25aa25f9fd5497e66
onepiecefreak3 <realonepiecefreak@gmx.de>
2022-05-29 18:58:10 +0200

    IsAfterTodaySpanningTime now calculates from PST Timezone correctly;

--- nier-rein-api/NierReincarnation.Core/Subsystem/Calculator/Outgame/CalculatorDateTime.cs
+++ nier-rein-api/NierReincarnation.Core/Subsystem/Calculator/Outgame/CalculatorDateTime.cs
@@ -1,10 +1,21 @@
 ﻿using System;
-using NierReincarnation.Core.Dark.StateMachine;
+using TimeZoneConverter;
 
 namespace NierReincarnation.Core.Subsystem.Calculator.Outgame
 {
     public static class CalculatorDateTime
     {
+        // CUSTOM: PST Timezone
+        private static readonly TimeZoneInfo PstTimezone = TZConvert.GetTimeZoneInfo("Pacific Standard Time");
+
+        public static bool IsAfterTodaySpanningTime(DateTimeOffset checkDateTime)
+        {
+            // CUSTOM: Logic to check if checkDateTime is after start of current day
+            var startOfDay = TimeZoneInfo.ConvertTime(DateTime.Now, PstTimezone).Date;
+            var check = TimeZoneInfo.ConvertTime(checkDateTime.DateTime, PstTimezone);
+            return check >= startOfDay;
+        }
+
         public static bool IsWithinThePeriod(long startUnixTime, long endUnixTime)
         {
             var now = UnixTimeNow();
@@ -23,11 +34,11 @@ namespace NierReincarnation.Core.Subsystem.Calculator.Outgame
             return DateTimeOffset.FromUnixTimeMilliseconds(unixTime);
         }
 
-        public static bool IsAfterTodaySpanningTime(DateTimeOffset checkDateTime)
-        {
-            // CUSTOM: Logic to check if checkDateTime is after start of current day
-            var startOfDay = FromUnixTime(UnixTimeNow());
-            return checkDateTime >= startOfDay.Date;
-        }
+        //public static bool IsAfterTodaySpanningTime(DateTimeOffset checkDateTime)
+        //{
+        //    // CUSTOM: Logic to check if checkDateTime is after start of current day
+        //    var startOfDay = FromUnixTime(UnixTimeNow());
+        //    return checkDateTime >= startOfDay.Date;
+        //}
     }
 }
