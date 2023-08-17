4b2ba3c44422d6681566236ebf634fc9a70e36fd
BillyCool <BillyCool@users.noreply.github.com>
2023-08-18 00:31:59 +1000

    Sped up execution of master db table export

--- src/NierReincarnation.Datamine/Command/Menu/ExportDatabaseTablesMenuCommand.cs
+++ src/NierReincarnation.Datamine/Command/Menu/ExportDatabaseTablesMenuCommand.cs
@@ -18,35 +18,36 @@ public class ExportDatabaseTablesMenuCommand : AbstractMenuCommand
 
     public override async Task ExecuteAsync()
     {
-        await ExportMasterDb();
-        //await ExportUserDb();
+        ExportMasterDb();
+        //ExportUserDb();
     }
 
-    private static async Task ExportMasterDb()
+    private static void ExportMasterDb()
     {
         string emptyString = string.Concat(Enumerable.Repeat(" ", 80));
         List<string> exportedTables = new();
 
-        foreach (var tableProp in typeof(DarkMasterMemoryDatabase).GetProperties())
+        typeof(DarkMasterMemoryDatabase).GetProperties().AsParallel().ForAll(async tableProp =>
         {
             Console.Write($"\r{emptyString}");
             Console.Write($"\rChecking {tableProp.Name}");
             var filePath = Path.Combine(Constants.DatabasePath, $"{tableProp.Name}.json");
-            var tempFilePath = Path.Combine(Constants.DatabasePath, Constants.TempFolder, $"{DataManager.Revision}", $"{tableProp.Name}.json");
+            var tempFilePath = Path.Combine(Constants.DatabasePath, Constants.TempFolder, $"{DatabaseDefine.Master.Version}", $"{tableProp.Name}.json");
             var tableValue = tableProp.GetValue(DatabaseDefine.Master, null);
             var allProp = tableProp.PropertyType.GetProperty("All");
+            if (allProp is null) return;
             var allValue = allProp.GetValue(tableValue, null);
 
             string json = JsonConvert.SerializeObject(allValue as IEnumerable, Formatting.Indented);
 
             // Skip file without changes
-            if (File.Exists(filePath) && await File.ReadAllTextAsync(filePath) == json) continue;
+            if (File.Exists(filePath) && await File.ReadAllTextAsync(filePath) == json) return;
 
             // Write file with changes
             Directory.CreateDirectory(Path.GetDirectoryName(tempFilePath));
             await File.WriteAllTextAsync(tempFilePath, json);
             exportedTables.Add(filePath);
-        }
+        });
         Console.Write($"\r{emptyString}");
         Console.WriteLine();
 
@@ -56,12 +57,12 @@ public class ExportDatabaseTablesMenuCommand : AbstractMenuCommand
         }
     }
 
-    private static async Task ExportUserDb()
+    private static void ExportUserDb()
     {
         string emptyString = string.Concat(Enumerable.Repeat(" ", 80));
         List<string> exportedTables = new();
 
-        foreach (var tableProp in typeof(DarkUserMemoryDatabase).GetProperties())
+        typeof(DarkUserMemoryDatabase).GetProperties().AsParallel().ForAll(async tableProp =>
         {
             Console.Write($"\r{emptyString}");
             Console.Write($"\rChecking {tableProp.Name}");
@@ -69,18 +70,19 @@ public class ExportDatabaseTablesMenuCommand : AbstractMenuCommand
             var tempFilePath = Path.Combine(Constants.DatabasePath, Constants.TempFolder, $"{DataManager.Revision}", $"{tableProp.Name}.json");
             var tableValue = tableProp.GetValue(DatabaseDefine.User, null);
             var allProp = tableProp.PropertyType.GetProperty("All");
+            if (allProp is null) return;
             var allValue = allProp.GetValue(tableValue, null);
 
             string json = JsonConvert.SerializeObject(allValue as IEnumerable, Formatting.Indented);
 
             // Skip file without changes
-            if (File.Exists(filePath) && await File.ReadAllTextAsync(filePath) == json) continue;
+            if (File.Exists(filePath) && await File.ReadAllTextAsync(filePath) == json) return;
 
             // Write file with changes
             Directory.CreateDirectory(Path.GetDirectoryName(tempFilePath));
             await File.WriteAllTextAsync(tempFilePath, json);
             exportedTables.Add(filePath);
-        }
+        });
         Console.Write($"\r{emptyString}");
         Console.WriteLine();
 
