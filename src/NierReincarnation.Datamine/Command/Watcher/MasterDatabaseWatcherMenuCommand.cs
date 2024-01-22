6ac2d8736bb95b05b740458a5299ba5c29b8e5c7
BillyCool <BillyCool@users.noreply.github.com>
2024-01-22 19:17:33 +1100

    Updated db watcher to only check the past 30 minutes

--- src/NierReincarnation.Datamine/Command/Watcher/MasterDatabaseWatcherMenuCommand.cs
+++ src/NierReincarnation.Datamine/Command/Watcher/MasterDatabaseWatcherMenuCommand.cs
@@ -8,8 +8,6 @@ public class MasterDatabaseWatcherMenuCommand(MasterDatabaseWatcherMenuCommandAr
 {
     private static readonly object _lockObj = new();
 
-    private static readonly int[] _hours = [/* 1, 2, 3, 4, 5, 14, */ 15, 16, 17, 18, 19, /* 20, 21, 22, 23 */];
-
     private const int _batchSize = 10000;
 
     public override bool IsActive => Program.AppSettings.IsSetup && Program.AppSettings.IsOfflineMode;
@@ -35,38 +33,13 @@ public class MasterDatabaseWatcherMenuCommand(MasterDatabaseWatcherMenuCommandAr
 
         var serverTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
         var serverTime = DateTimeOffset.UtcNow.Add(serverTimeZone.BaseUtcOffset);
-        var twoDaysAgo = serverTime.AddDays(-2);
+        var startDate = serverTime.AddMinutes(-30);
 
-        int[] months = new[] { twoDaysAgo.Month, serverTime.Month }.Distinct().ToArray();
-
-        Dictionary<int, int[]> days = [];
-
-        if (months.Length == 2)
+        while (startDate <= serverTime)
         {
-            days.Add(months[0], Enumerable.Range(twoDaysAgo.Day, DateTime.DaysInMonth(serverTime.Year, months[0]) - twoDaysAgo.Day + 1).ToArray());
-            days.Add(months[1], Enumerable.Range(1, serverTime.Day).ToArray());
-        }
-        else
-        {
-            days.Add(months[0], Enumerable.Range(twoDaysAgo.Day, serverTime.Day - twoDaysAgo.Day + 1).ToArray());
-        }
-
-        foreach (var month in months)
-        {
-            for (int dayIndex = 0; dayIndex < days[month].Length; dayIndex++)
-            {
-                for (int hourIndex = 0; hourIndex < _hours.Length; hourIndex++)
-                {
-                    for (int minute = 1; minute <= 60; minute++)
-                    {
-                        for (int second = 1; second <= 60; second++)
-                        {
-                            string masterVersion = $"{serverTime.Year}{month:D2}{days[month][dayIndex]:D2}{_hours[hourIndex]:D2}{minute:D2}{second:D2}";
-                            masterVersions.Add(masterVersion);
-                        }
-                    }
-                }
-            }
+            string masterVersion = $"{serverTime.Year}{startDate.Month:D2}{startDate.Day:D2}{startDate.Hour:D2}{startDate.Minute:D2}{startDate.Second:D2}";
+            masterVersions.Add(masterVersion);
+            startDate = startDate.AddSeconds(1);
         }
 
         List<List<string>> batches = [];
